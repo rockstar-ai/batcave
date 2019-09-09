2d4d195104e651c334334f846a1b1075c627f487
rockstar-ai <rockstar-ai@github.com>
2019-09-08 22:07:21 -0700

    Until Cast Complete

--- Python projects/new.py
+++ Python projects/new.py
@@ -50,30 +50,29 @@ data["Movie Name"]= new[0]
 data["Rest Data"]= new[2] 
 data["Cast Info"] = ""
 print(data)
+totalmovie=len(data["Movie Name"])
+print(totalmovie)
 
 #looping through dataframe's movie name column 
-i=3
-j=2
-for entry in data.iloc[0]:
-    search_results = ia.search_movie(entry)
-    if search_results:
-     movieID = search_results[0].movieID
-     movie = ia.get_movie(movieID)
-     print(movie)
-     if movie:
-         cast = movie.get('cast')
-         actoriter=0
-         topActors = 5
-         list=[]
-         
-         for actor in cast[actoriter:topActors]:
-            cast=("{0} as {1}".format(actor['name'], actor.currentRole))
-            print(cast)
-            list.extend([cast])
-            actoriter=actoriter+1
-            data.at[i,'Cast Info'] = list
-            
-    
-    
 
+for i in range (0,totalmovie,1):
+    for entry in data.iloc[i]:
+        search_results = ia.search_movie(entry)
+        if search_results:
+            movieID = search_results[0].movieID
+            movie = ia.get_movie(movieID)
+            print(movieID," : ",movie)
+            if movie:
+                cast = movie.get('cast')
+                actoriter=0
+                topActors = 5
+                emptylist=[]
+        
+                for actor in cast[actoriter:topActors]:
+                    cast=("{0} as {1}".format(actor['name'], actor.currentRole))
+                    print(cast)
+                    emptylist.extend([cast])
+                    actoriter=actoriter+1
+            data.at[i,'Cast Info'] = emptylist
+            
 data.to_csv(r'C:/Users/GamerTech/Desktop/sample2.csv',index = None, header=True)
